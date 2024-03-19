f703a26923987102508efa92c5739a9cdbaaf09e
Edisheri <145288760+Edisheri@users.noreply.github.com>
2024-03-19 10:17:28 +0400

    Delete Documents/drf_h_w directory

--- Documents/drf_h_w/materials/validators.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.serializers import ValidationError
-
-
-def url_validator(url):
-    if url and 'youtube.com' not in url:
-        raise ValidationError('Недопустимая ссылка на сторонний ресурс')
--- Documents/drf_h_w/users/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from users.models import User, Payment
-
-admin.site.register(User)
-
-admin.site.register(Payment)
